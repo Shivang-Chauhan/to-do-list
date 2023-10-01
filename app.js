6b55c470ee8ebbabd2511641f5e9aad1fbfc8254
Shivang-Chauhan <135962677+Shivang-Chauhan@users.noreply.github.com>
2023-10-01 12:19:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
+app.options('*', cors());
 
 
+// app.get("/",cors(),(req,res)=>{
 
-app.get("/",cors(),(req,res)=>{
-
-})
+// })
 
 
 app.post("/",async(req,res)=>{
